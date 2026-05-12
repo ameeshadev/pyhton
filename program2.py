c2ee92cc569eb1bcf55afb51188051d36ba42444
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-11 22:12:13 -0700

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,12 @@
+#calculating simple intrest
+Principal=2000
+Time=3
+RateofIntrest=4
+SimpleIntrest=(Principal+Time+RateofIntrest)/100
+print(SimpleIntrest)
+#Average of Three Numbers
+x=100
+y=350
+z=540
+Average=(x+y+z)/3
+print(Average)
