7263e9e609781f2a175fe2c8bcfa4398a2ec204e
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-11 21:15:08 -0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,21 @@
+x=10
+y="ameesha"
+a=int(3)
+b=str(3)
+c=float(3)
+#using casting function
+print(x)
+print(y)
+print(a)
+print(b)
+print(c)
+#using type() function
+d=30.00
+e="ameeshadev"
+print(type(d))
+print(type(e))
+#using case sensitive python variables are case sensitive
+A=130
+a="ammu"
+print(A)
+print(a)
