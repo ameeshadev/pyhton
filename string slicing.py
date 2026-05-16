0643a3b1b7bd7bd2738c0cfcf068708712ab1ff7
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-15 22:17:32 -0400

    Create string slicing.py

---
+++ string slicing.py
@@ -0,0 +1,12 @@
+string="ameesha is learning python"
+#slicing index
+print(string[3:7])
+
+# slicing at the start
+print(string[:8])
+
+# slicing at the end
+print(string[1:])
+
+# negative slicing
+print(string[-9:-2])
