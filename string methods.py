dc8b1e73cf6eb6d1a550a66f40c6813ab8423df1
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-15 22:36:54 -0400

    Add examples of string methods in Python
    Demonstrate various string methods including upper, lower, replace, strip, and split.

---
+++ string methods.py
@@ -0,0 +1,15 @@
+# string methods
+# upper() method
+a="ameesha"
+print(a.upper())
+# lower() method
+b="PROGRAMMING"
+print(b.lower())
+# replacing the string
+c=" Hello World! "
+print(c.replace("H","A"))
+# removing of whitespace by using split() method
+print(c.strip())
+# split() ethod
+x="pyhton is easy"
+print(x.split())
