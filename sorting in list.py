05255158442fe35434b8b246e62af0bf0770dadd
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-17 02:51:29 -0400

    Create sorting in list.py

---
+++ sorting in list.py
@@ -0,0 +1,10 @@
+# sorting a list:
+# It sorting the list automatically assending,by default
+list1=["paper","books","songs","study"]
+list1.sort()
+print(list1)
+
+# sorting for descending order:
+# we use keyword argument reverse=true
+list1.sort(reverse=True)
+print(list1)
