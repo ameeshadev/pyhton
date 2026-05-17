4cbbc6a3b21bc3a14ed1358861b33f3b731cf122
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-17 02:31:50 -0400

    Create list datatype.py

---
+++ list datatype.py
@@ -0,0 +1,16 @@
+# list datatype
+list1=["Honey","Mahir","Erfan","Imran","Ameesha"]
+
+print(list1)
+# using slicing method
+print(list1[1:3:])
+print(list1[:-1])
+# Adding a list
+list1.append("ammu")
+print(list1)
+# accesing the list 
+list2=["apple","banana","mango","grapes"]
+print(list2[2])
+# change the list
+list2[0]="fruit"
+print(list2)
