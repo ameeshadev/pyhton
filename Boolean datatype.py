e6e359a8f6e7ffcc8912b946ddfe059d55ba2352
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-17 01:28:11 -0400

    Add Boolean datatype example with comparison
    Implement a simple comparison using Boolean datatype.

---
+++ Boolean datatype.py
@@ -0,0 +1,9 @@
+# Boolean Datatype
+a=10
+b=7
+# Boolean Datatype represent by using conditions:if,else
+if a<b:
+    print("Yes a is Smaller Than b")
+    
+else:
+    print("No a is Bigger Than b")
