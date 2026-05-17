665f1829203bd56399ccd84b2a2bd73312a6f4c9
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-17 02:43:10 -0400

    Create list datatype1.py

---
+++ list datatype1.py
@@ -0,0 +1,15 @@
+# list datatype
+list1=["Honey","Mahir","Erfan","Imran","Ameesha"]
+
+# removing list
+list1.remove("Ameesha")
+print(list1)
+# by using pop() method we delete or remove the list
+list1.pop(3)
+print(list1)
+# for deleting list we use DEL keyword
+del list1
+# clear the list
+list2=["Book","Pen","Pencil"]
+list2.clear()
+print(list2)
