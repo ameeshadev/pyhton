9c8f5d589b7a0134019e5fd0fd2697b13909aa52
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-17 21:49:16 -0400

    Create tuple datatype.py

---
+++ tuple datatype.py
@@ -0,0 +1,11 @@
+# Tuple Datatype
+tuple1=("apple","banana","orange","grapes")
+print(tuple1)
+# slicing
+# Positive slicing
+print(tuple1[0:2])
+# negative slicing
+print(tuple1[-3:-2])
+# For adding two tuples we use + operator
+tuple2=("1","2","3","4")
+print(tuple1+tuple2)
