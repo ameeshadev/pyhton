eae667203adf4a07852d1b332477c9073c78e044
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-20 04:43:23 -0400

    Create Neasted-for loop.py

---
+++ Neasted-for loop.py
@@ -0,0 +1,6 @@
+# Nestead-for loop
+categories=['fruit ','vegetable ']
+foods=['apple','mango','tomato','carrot']
+for categorie in categories:
+    for food in foods:
+        print(categorie,food)
