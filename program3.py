27b1c9368a82be0a8009ca960da3a77bf6713b5d
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-20 05:04:07 -0400

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,11 @@
+# while loop:
+# this loop will repeat a block of code until the condition is False
+Your_Name = ""
+
+while Your_Name == "":
+    Your_Name = input("Enter a Name: ")
+
+Noof_Letters = len(Your_Name)
+
+print("Your name is:", Your_Name)
+print("Number of letters:", Noof_Letters)
