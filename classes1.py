c520b748612f0aa226d85c964b64ecfbb978b6bf
AMEESHA SHAIK <140043042+ameeshadev@users.noreply.github.com>
2026-05-29 06:18:16 -0400

    Add Car class with color and model attributes

---
+++ classes1.py
@@ -0,0 +1,13 @@
+lass Car:
+    def __init__(self, color, model):
+        self.color = color
+        self.model = model
+
+car_1 = Car("red", "Toyota Corolla")
+car_2 = Car("green", "Lamborghini Revuelto")
+
+print(car_1.model) # Toyota Corolla
+print(car_2.model) # Lamborghini Revuelto
+
+print(car_1.color) # red
+print(car_2.color) # green
